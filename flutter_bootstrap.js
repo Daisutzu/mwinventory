7cacdd26b16e395784aeacda1dc44c718d361b64
Daisutzu <Daisutzu@users.noreply.github.com>
2026-09-20 14:40:45 +0000

    deploy: e8ea9f8b771c4c381ddc7e5c85ed06e7ee8010ba

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1257745210" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1231035655" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
